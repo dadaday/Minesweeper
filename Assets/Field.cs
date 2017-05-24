d3247e942854bdde72a28f2d11ea42fdcc397ec5
dadaday <aaurub@gmail.com>
2017-05-24 07:41:57 +0600

    added field, some image assets, added mouse interaction
    TODO: Pretty much everything else

---
+++ Assets/Field.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Field {
+	private int NUM;
+	private int MINES;
+	private int[,] fieldArr;
+
+	public Field(int n, int m) {
+		NUM = n;
+		MINES = m;
+		fieldArr = new int[NUM, NUM];
+
+		for (int x = 0; x < NUM; x++) {
+			for (int y = 0; y < NUM; y++) {
+				fieldArr [x, y] = 0;
+			}
+		}
+
+		int count = MINES;
+
+		while (count > 0) {
+			int x = Random.Range (0, NUM);
+			int z = Random.Range (0, NUM);
+
+			if (fieldArr [x, z] == 0) {
+				fieldArr [x, z] = -1;
+				count--;
+				Debug.Log ("MINIFIED " + x + "-" + z);
+			}
+		}
+	}
+
+
+	public bool isTileBomb(int x, int z) {
+		if (fieldArr [x, z] == -1) {
+			return true;
+		}
+		return false;
+	}
+}
---
+++ Assets/MinesweeperScript.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MinesweeperScript : MonoBehaviour {
+	private Field field;
+
+	public int NUM = 9;
+	public int MINES = 10;
+
+	public GameObject cellPrefab;
+	public GameObject mineCellPrefab;
+	public RawImage smileImage;
+	public Texture[] textures;
+
+	private float cellWidth = 1.0f;
+	private int DIM;
+	private bool finished;
+
+	private GameObject[,] cells;
+	private Vector3 clickedCoord;
+
+	private void drawTable(int num) {
+
+		cellPrefab.transform.localScale = new Vector3(cellWidth, 0.1f, cellWidth);
+
+		for (int row = 0; row < num; ++row) {
+			for (int col = 0; col < num; ++col) {
+				cells[row, col] = (GameObject) Instantiate (cellPrefab, new Vector3 (row * cellWidth, 0, col * cellWidth), Quaternion.identity);
+			}
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		DIM = NUM * NUM;
+		cells = new GameObject[NUM, NUM];
+
+		reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!finished && Input.GetMouseButtonDown (0)) {
+			RaycastHit hitInfo = new RaycastHit ();
+
+			bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo);
+			if (hit) {
+				GameObject selectedGO = hitInfo.transform.gameObject;
+
+				if (selectedGO.tag == "CellTag") {
+					clickedCoord = selectedGO.GetComponent<Transform> ().position;
+					int x = (int)clickedCoord.x;
+					int z = (int)clickedCoord.z;
+					Debug.Log ("x: " + x + " z: " + z);
+					Destroy (cells [x, z]);
+					if (field.isTileBomb (x, z)) {
+						kaboom (x, z);
+					}
+				}
+			} 
+
+		}
+	}
+
+	private void kaboom(int x, int z) {
+		cells[x,z] = (GameObject) Instantiate (mineCellPrefab, new Vector3 (x * cellWidth, 0, z * cellWidth), Quaternion.identity);
+		finished = true;
+		smileImage.texture = textures [2];
+	}
+
+	public void reset() {
+		smileImage.texture = textures [0];
+
+		foreach (GameObject cell in cells) {
+			Destroy (cell);
+		}
+
+		field = new Field (NUM, MINES);
+		finished = false;
+		drawTable (NUM);
+	}
+
+	public void ExitApplication() {
+		Application.Quit ();
+	}
+}
