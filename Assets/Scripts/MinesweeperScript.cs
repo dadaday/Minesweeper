a977b92b737f12b6f0af39b5a0a90e0c9ac1ca16
dadaday <aaurub@gmail.com>
2017-05-24 12:34:43 +0600

    some :poop:

--- Assets/Scripts/MinesweeperScript.cs
+++ Assets/Scripts/MinesweeperScript.cs
@@ -14,6 +14,9 @@ public class MinesweeperScript : MonoBehaviour {
 	public Texture[] textures;
 	public Texture[] mineCountTextures;
 
+	public Text mineCountText;
+	public Text timerCounter;
+
 	private float cellWidth = 1.0f;
 	private int DIM;
 	private bool finished;
@@ -23,6 +26,8 @@ public class MinesweeperScript : MonoBehaviour {
 
 	private GameObject[,] cells;
 	private Vector3 clickedCoord;
+	private bool[,] isFlagged;
+	private bool[,] isOpened;
 
 	private void drawTable(int num) {
 
@@ -31,6 +36,8 @@ public class MinesweeperScript : MonoBehaviour {
 		for (int row = 0; row < num; ++row) {
 			for (int col = 0; col < num; ++col) {
 				cells[row, col] = (GameObject) Instantiate (cellPrefab, new Vector3 (row * cellWidth, 0, col * cellWidth), Quaternion.identity);
+				isFlagged [row, col] = false;
+				isOpened [row, col] = false;
 			}
 		}
 	}
@@ -45,30 +52,63 @@ public class MinesweeperScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!finished && Input.GetMouseButtonDown (0)) {
-			RaycastHit hitInfo = new RaycastHit ();
+		if (!finished) {
+			if (Input.GetMouseButtonDown (0)) {
+				RaycastHit hitInfo = new RaycastHit ();
 
-			bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo);
-			if (hit) {
-				GameObject selectedGO = hitInfo.transform.gameObject;
+				bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo);
+				if (hit) {
+					GameObject selectedGO = hitInfo.transform.gameObject;
 
-				if (selectedGO.tag == "CellTag") {
-					clickedCoord = selectedGO.GetComponent<Transform> ().position;
-					int x = (int)clickedCoord.x;
-					int z = (int)clickedCoord.z;
+					if (selectedGO.tag == "CellTag") {
+						clickedCoord = selectedGO.GetComponent<Transform> ().position;
+						int x = (int)clickedCoord.x;
+						int z = (int)clickedCoord.z;
 
-					if (field.getStateOf (x, z) == Field.STATE.NOTVISITED) {
-						if (field.isTileBomb (x, z)) {
-							kaboom (x, z);
+						if (field.getStateOf (x, z) == Field.STATE.NOTVISITED) {
+							if (field.isTileBomb (x, z)) {
+								kaboom (x, z);
+							}
 						}
 					}
 				}
-			} 
+			}
+			if (Input.GetMouseButtonDown (1)) {
+				RaycastHit hitInfo = new RaycastHit ();
 
+				bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo);
+				if (hit) {
+					GameObject selectedGO = hitInfo.transform.gameObject;
+
+					if (selectedGO.tag == "CellTag") {
+						clickedCoord = selectedGO.GetComponent<Transform> ().position;
+						int x = (int)clickedCoord.x;
+						int z = (int)clickedCoord.z;
+						if (!isOpened [x, z]) {
+							Texture flagOrEmpty;
+							int minesC = int.Parse(mineCountText.text);
+							if (isFlagged [x, z]) {
+								flagOrEmpty = textures [4];
+								minesC++;
+							} else {
+								flagOrEmpty = textures [3];
+								minesC--;
+							}
+							mineCountText.text = minesC.ToString ();
+							isFlagged [x, z] = !isFlagged [x, z];
+							cells [x, z].gameObject.GetComponent<MeshRenderer> ().materials [0].SetTexture ("_MainTex", flagOrEmpty);
+						}
+					}
+				}
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			reset();
 		}
 	}
 
 	public void openTile(int x, int z, int minesAround) {
+		isOpened [x, z] = true;
 		openedTiles++;
 		Debug.Log ("OPENED: " + openedTiles);
 		cells [x, z].gameObject.GetComponent<MeshRenderer>().materials[0].SetTexture("_MainTex", mineCountTextures[minesAround]);
@@ -83,13 +123,16 @@ public class MinesweeperScript : MonoBehaviour {
 	}
 
 	private void kaboom(int x, int z) {
-		cells[x,z] = (GameObject) Instantiate (mineCellPrefab, new Vector3 (x * cellWidth, 0, z * cellWidth), Quaternion.identity);
+		cells [x, z].gameObject.GetComponent<MeshRenderer>().materials[0].SetTexture("_MainTex", textures[5]);
 		finished = true;
 		smileImage.texture = textures [2];
 	}
 
 	public void reset() {
+		mineCountText.text = MINES.ToString ();
 		DIM = NUM * NUM;
+		isFlagged = new bool[NUM, NUM];
+		isOpened = new bool[NUM, NUM];
 
 		smileImage.texture = textures [0];
 
